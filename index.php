0a2a21fecbad12236382e7e0440989ab69520600
kumarabh1 <96345153+kumarabh1@users.noreply.github.com>
2023-10-25 18:24:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -163,7 +163,7 @@ if(isset($_POST['name'])){
         //   echo "The value of Array are ".$variable4[1]."<br>";
         //   echo "The value of Array are ".$variable4[2]."<br>";
         //   echo "The value of Array are ".$variable4[3]."<br>";
-        //   echo "The value of Array are ".$variable4[4]."<br>";
+       
        
         
        
